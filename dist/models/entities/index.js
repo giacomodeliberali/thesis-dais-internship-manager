10d1429451373adac40d0f4229db237d38bafba6
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2018-03-27 16:22:25 +0200

    Update models index exports

--- dist/models/entities/index.js
+++ dist/models/entities/index.js
@@ -3,6 +3,7 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./base"));
 __export(require("./company.model"));
 __export(require("./internship.model"));
 __export(require("./internship-proposal.model"));
--- src/models/entities/index.ts
+++ src/models/entities/index.ts
@@ -1,3 +1,4 @@
+export * from './base';
 export * from './company.model';
 export * from './internship.model';
 export * from './internship-proposal.model';
